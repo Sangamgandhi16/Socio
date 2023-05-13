9b3658b195b53f99bc910f6cd396f25ab11b2a02
Sangamgandhi16 <sangamgandhi16@gmail.com>
2023-05-14 01:13:26 +0530

    added card view for products

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import Navbar from "react-bootstrap/Navbar";
@@ -8,7 +8,7 @@ import { LinkContainer } from "react-router-bootstrap";
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className="site-container">
         <Navbar bg="dark" variant="dark">
           <Container>
             <LinkContainer to="/">
@@ -18,11 +18,16 @@ function App() {
         </Navbar>
 
         <main>
-          <Routes>
-            <Route path="/product/:slug" element={<ProductScreen />} />
-            <Route path="/" element={<HomeScreen />} />
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className="text-center">All Rights reserved</div>
+        </footer>
       </div>
     </BrowserRouter>
   );
